a73adca19ff2b7fbd8d220288e9ad113c6561cb2
Gerard Skomin <gerard.skomin@gmail.com>
2022-02-08 15:58:43 +0100

    Last changes

--- src/com/gerskom/InitialFrame.java
+++ src/com/gerskom/InitialFrame.java
@@ -16,15 +16,15 @@ public class InitialFrame extends JFrame {
     JPanel eastPanel = new JPanel();
     JPanel centerPanel = new JPanel();
 
-    JTextField gameWidth = new JTextField("400");
-    JTextField gameHeight = new JTextField("300");
+    JTextField gameWidth = new JTextField("500");
+    JTextField gameHeight = new JTextField("400");
     JButton startButton = new JButton("PLAY");
 
     InitialFrame() {
         int frameWidth = 600;
         int frameHeight = 400;
         this.setSize(frameWidth, frameHeight);
-        this.setTitle("Forest Fire Simulation");
+        this.setTitle("Fluid Flow Simulation");
         this.getContentPane().setBackground(Color.DARK_GRAY);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
